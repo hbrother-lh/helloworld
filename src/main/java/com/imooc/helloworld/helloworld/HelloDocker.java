cd8f76b01016afa7f71f8cd98acf5a729a4befd9
dfcc <eureka_burning@163.com>
2020-06-17 11:41:33 +0800

    helloDocker

--- src/main/java/com/imooc/helloworld/helloworld/HelloDocker.java
+++ src/main/java/com/imooc/helloworld/helloworld/HelloDocker.java
@@ -12,6 +12,6 @@ public class HelloDocker {
     @RequestMapping("/hello")
     @ResponseBody
     public String hello() {
-        return "helloDockerNgrok";
+        return "helloDockerNgrokWhy";
     }
 }
