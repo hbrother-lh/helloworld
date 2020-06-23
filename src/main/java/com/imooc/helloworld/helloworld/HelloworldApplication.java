0fab96953a217965717d840a52605af599be0ac2
hbrother-lh <eureka_burning@163.com>
2020-06-23 10:29:17 +0800

    helloDocker

--- src/main/java/com/imooc/helloworld/helloworld/HelloworldApplication.java
+++ src/main/java/com/imooc/helloworld/helloworld/HelloworldApplication.java
@@ -2,16 +2,14 @@ package com.imooc.helloworld.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class HelloworldApplication {
 
 	public static void main(String[] args) {
-		try {
-			SpringApplication.run(HelloworldApplication.class, args);
-		}catch (Exception e){
-			e.printStackTrace();
-		}
+	    SpringApplication.run(HelloworldApplication.class, args);
 	}
 
 }
