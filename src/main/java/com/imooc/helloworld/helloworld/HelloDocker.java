3dcf51e68a44e8e156f05d22aa8c18bf70a8b01f
hbrother-lh <eureka_burning@163.com>
2020-06-23 10:29:10 +0800

    helloDocker

--- src/main/java/com/imooc/helloworld/helloworld/HelloDocker.java
+++ src/main/java/com/imooc/helloworld/helloworld/HelloDocker.java
@@ -1,7 +1,7 @@
 package com.imooc.helloworld.helloworld;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloDocker {
 
-    @RequestMapping("/hello")
+    @RequestMapping("/hello/{name}")
     @ResponseBody
-    public String hello() {
-        return "helloDockerNgrokWhy";
+    public String hello(@PathVariable("name")String name) {
+        return "helloDockerNgrokWhy"+name;
     }
 }
