43970078b73d5e17afe01b8e7fe12ee4b6a6fc3f
hbrother-lh <eureka_burning@163.com>
2020-06-29 17:56:42 +0800

    helloDocker

---
+++ src/main/java/com/imooc/javabean/User.java
@@ -0,0 +1,31 @@
+package com.imooc.javabean;
+
+public class User {
+    public  int id;
+    public  String name;
+    public  String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
