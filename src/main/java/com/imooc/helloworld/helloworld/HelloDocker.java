793ea7dbd8650a87b663e38781d2e5c1395c7885
hbrother-lh <eureka_burning@163.com>
2020-06-30 10:38:27 +0800

    helloDocker

--- src/main/java/com/imooc/helloworld/helloworld/HelloDocker.java
+++ src/main/java/com/imooc/helloworld/helloworld/HelloDocker.java
@@ -8,7 +8,6 @@ import com.imooc.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
